1720a1f674985e6d5ab83dd8424c50c5e0f63340
RicardoP0 <ricardo1459@gmail.com>
2022-09-25 09:23:17 -0300

    fix: data aug shift using pytorch format

--- data_transforms/ripple_spect_trs.py
+++ data_transforms/ripple_spect_trs.py
@@ -1,13 +1,28 @@
 import torch
 import random
 
-def shift_cutoff(x,signal_size=0.150,max_shift_s=0.015,fs=600):    
-    #randomly move the spectrogram +- shift_ms
-    shift_p = int(max_shift_s*fs)
-    shift_val = random.randint(-shift_p,shift_p)
-    x = torch.roll(x,shifts=shift_val,dims=1)
-    x_size = x.shape[1]
-    #obtain a signal of signal_size
-    signal_size_p = int(signal_size*fs)
-    x = x[:,x_size//2-signal_size_p//2:x_size//2+signal_size_p//2]    
-    return x
+class SpectShiftCutoff(object):
+    """Shift and cutoff the spectrogram in time and frequency.
+    Args:
+        shift (int): how much to shift the spectrogram in time
+        cutoff (int): how much to cutoff the spectrogram in frequency
+    """
+
+    def __init__(self, signal_size=0.150,max_shift_s=0.015,fs=600):
+        self.signal_size = signal_size
+        self.max_shift_s = max_shift_s
+        self.fs = fs
+
+    def __call__(self, x):
+        #randomly move the spectrogram +- shift_ms
+        shift_p = int(self.max_shift_s*self.fs)
+        shift_val = random.randint(-shift_p,shift_p)
+        x = torch.roll(x,shifts=shift_val,dims=1)
+        x_size = x.shape[1]
+        #obtain a signal of signal_size
+        signal_size_p = int(self.signal_size*self.fs)
+        x = x[:,x_size//2-signal_size_p//2:x_size//2+signal_size_p//2]    
+        return x
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(signal_size={0}, cutoff={1})'.format(self.signal_size, self.max_shift_s)
